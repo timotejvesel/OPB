cf7c40006ef81c3c10f6a8679dcdd3e8c13a3c7d
Martin Praček <mpracek299@gmail.com>
2019-05-23 14:08:10 +0200

    preimenovanja

--- shiny/server.R
+++ shiny/server.R
@@ -2,7 +2,6 @@
 #Najprej zaženi datoteko libraries.r
 
 source("../auth_public.r")
-source("serverFunctions.R")
 
 #tukaj klici sql, ki se povezejo na ui.R
 
@@ -11,134 +10,11 @@ shinyServer(function(input,output,session) {
   drv <- dbDriver("PostgreSQL") 
   conn <- dbConnect(drv, dbname = db, host = host,
                     user = user, password = password)
-  userID <- reactiveVal()    # Placeholder za userID
-  loggedIn <- reactiveVal(FALSE)    # Placeholder za logout gumb oz vrednost gumba
-  
   dbGetQuery(conn, "SET CLIENT_ENCODING TO 'utf8'; SET NAMES 'utf8'")
   
   cancel.onSessionEnded <- session$onSessionEnded(function() {
     dbDisconnect(conn) #ko zapremo shiny naj se povezava do baze zapre
   })
-  output$signUpBOOL <- eventReactive(input$signup_btn, 1)
-  outputOptions(output, 'signUpBOOL', suspendWhenHidden=FALSE)  # Da omogoca skrivanje/odkrivanje
-  observeEvent(input$signup_btn, output$signUpBOOL <- eventReactive(input$signup_btn, 1))
-  
-  # Greyout of signin button
-  observeEvent(c(input$userName,input$password), {
-    shinyjs::toggleState("signin_btn", 
-                         all(c(input$userName, input$password)!=""))
-  })
-  
-  # Sign in protocol
-  observeEvent(input$signin_btn,
-               {signInReturn <- sign.in.user(input$userName, input$password)
-               if(signInReturn[[1]]==1){
-                 userID(signInReturn[[2]])
-                 output$signUpBOOL <- eventReactive(input$signin_btn, 2)
-                 loggedIn(TRUE)
-               }else if(signInReturn[[1]]==0){
-                 showModal(modalDialog(
-                   title = "Error during sign in",
-                   paste0("An error seems to have occured. Please try again."),
-                   easyClose = TRUE,
-                   footer = NULL
-                 ))
-               }else{
-                 showModal(modalDialog(
-                   title = "Wrong Username/Password",
-                   paste0("Username or/and password incorrect"),
-                   easyClose = TRUE,
-                   footer = NULL
-                 ))
-               }
-               })
-  
-  # Greyout of signup button
-  observeEvent(c(input$SignUpName, input$SignUpSurname, input$SignUpAddress, input$SignUpCity,
-                 input$SignUpCountry, input$SignUpEmso, input$SignUpMail, 
-                 input$SignUpUserName, input$SignUpPassword), {
-                   shinyjs::toggleState("signup_btnSignUp",
-                                        all(c(input$SignUpName, input$SignUpSurname, input$SignUpAddress, input$SignUpCity,
-                                              input$SignUpCountry, input$SignUpEmso, input$SignUpMail, 
-                                              input$SignUpUserName, input$SignUpPassword)!="")# & 
-                                        # Preveri, ce samo latin characterji
-                                        # !any(grepl("[^\x20-\x7F]",
-                                        #         c(input$SignUpName, input$SignUpSurname, input$SignUpAddress, input$SignUpCity,
-                                        #           input$SignUpCountry, input$SignUpEmso, input$SignUpMail, 
-                                        #           input$SignUpUserName, input$SignUpPassword)))
-                   )
-                 })
-  
-  # Sign up protocol
-  observeEvent(input$signup_btnSignUp,
-               {
-                 if(any(grepl("[^\x20-\x7F]",
-                              c(input$SignUpName, input$SignUpSurname, input$SignUpAddress, input$SignUpCity,
-                                input$SignUpCountry, input$SignUpEmso, input$SignUpMail,
-                                input$SignUpUserName, input$SignUpPassword)))){
-                   success <- -1
-                 }else{
-                   success <- sign.up.user(input$SignUpName, input$SignUpSurname, input$SignUpAddress, 
-                                           input$SignUpCity, input$SignUpCountry, input$SignUpEmso,
-                                           input$SignUpMail, input$SignUpUserName, input$SignUpPassword)
-                 }
-                 if(success==1){
-                   showModal(modalDialog(
-                     title = "You have successfully signed up!",
-                     paste0("Now you can login as ",input$SignUpUserName,''),
-                     easyClose = TRUE,
-                     footer = NULL
-                   ))
-                   output$signUpBOOL <- eventReactive(input$signup_btnSignUp, 0) 
-                 }else if(success==-10){
-                   showModal(modalDialog(
-                     title = "Username conflict!",
-                     paste0("The username ",input$SignUpUserName,' is already taken. Please,
-                            chose a new one.'),
-                     easyClose = TRUE,
-                     footer = NULL
-                     ))
-                 }else if(success==-1){
-                   showModal(modalDialog(
-                     title = "Signup unsuccessful",
-                     paste0("Only Latin characters allowed"),
-                     easyClose = TRUE,
-                     footer = NULL
-                   ))
-                 }else{
-                   showModal(modalDialog(
-                     title = "Error during sign up",
-                     paste0("An error seems to have occured. Please try again."),
-                     easyClose = TRUE,
-                     footer = NULL
-                   ))
-                 }
-               })
-  
-  # Back button to sign in page
-  observeEvent(input$signup_btnBack, output$signUpBOOL <- eventReactive(input$signup_btnBack, 0))
-  
-  # Login/logout button in header
-  observeEvent(input$dashboardLogin, {
-    if(loggedIn()){
-      output$signUpBOOL <- eventReactive(input$signin_btn, 0)
-      userID <- reactiveVal()
-    }
-    loggedIn(ifelse(loggedIn(), FALSE, TRUE))
-  })
-  
-  output$logintext <- renderText({
-    if(loggedIn()) return("Logout here.")
-    return("Login here")
-  })
-  
-  output$dashboardLoggedUser <- renderText({
-    if(loggedIn()) return(paste("Welcome,", pridobi.ime.uporabnika(userID())))
-    return("")
-  })
-  
-  
-  
   
 # -------------------------------------------------------------------------------------------------
   
@@ -226,25 +102,6 @@ output$voj <- DT::renderDataTable(DT::datatable({ #glavna tabela rezultatov
 }) %>% DT::formatDate(c('zacetek', 'konec'), method = "toLocaleDateString")) # datum v normalno obliko
                                                                             # +  pravilno sortiranje
 })
-
-
-# -------------------------------------------------------------------------------------------------
-#komentarji 
-mnenje <- renderText({input$komentar})
-sql2 <- build_sql("INSERT INTO komentar (id,uporabnik_ime,vojna_id, besedilo,cas)
-                  VALUES(clan,",input$vojna,",", mnenje, ",NOW()")
-data2 <- dbGetQuery(conn, sql2)
-
-najdi.komentar <- reactive({
-  validate(need(!is.null(input$vojna), "Izberi vojno!"))
-  sql_komentar <- build_sql("SELECT * FROM komentar
-                            WHERE vojna_id =",input$vojna)
-})
-  
-
-output$komentiranje <- DT::renderDataTable((DT::datatable(tabela2=najdi.komentar())))
-
-
 
 # -------------------------------------------------------------------------------------------------
 
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,80 +1,37 @@
-vpisniPanel <- tabPanel("SignIn", value="signIn",
-                         fluidPage(
-                           fluidRow(
-                             column(width = 12,
-                                    align = "center",
-                                    textInput("userName","User name", value= ""),
-                                    passwordInput("password","Password", value = ""),
-                                    actionButton("signin_btn", "Sign In"),
-                                    actionButton("signup_btn", "Sign Up"))
-                              )))
+library(shiny)
+library(shinythemes)
 
-registracijaPanel <- tabPanel("SignUp", value = "signUp",
-                              fluidPage(
-                                fluidRow(
-                                  column(width = 12,
-                                         align="center",
-                                         textInput("SignUpUserName","* Username", value= "", placeholder = "Only Latin characters."),
-                                         passwordInput("SignUpPassword","* Password", value= "", placeholder = "Only Latin characters."),
-                                         actionButton("signup_btnBack", "Back"),
-                                         actionButton("signup_btnSignUp", "Sign Up")
-                                  )
-                                )
-                              )
-                            )
-
-
-
-sidebar <- dashboardSidebar(hr(),
-  sidebarMenu(id="drzave",
-              menuItem("Pregled vojn po skupinah", tabName = "drzave", selected = TRUE)),
-  sidebarMenu(id="vojne",
-              menuItem("Pregled vojn",tabName = "vojne")),
-  sidebarMenu(id="stat", 
-              menuItem("Statistika po vojnah", tabName = "stat")),
-  sidebarMenu(id="kometnar", 
-              menuItem("Civilen diskurz o vojnah", tabName = "koment"))
-  
-)
-
-body <- dashboardBody(
-  tabItems(
-    tabItem(tabName = "drzave",
-            fluidRow(sidebarPanel(
-              uiOutput("izbor.sodelujoci")
-            ),
-            mainPanel(DT::dataTableOutput("sodel")
-              ))),
-    tabItem(tabName = "vojne",
-            fluidRow(sidebarPanel(
-              uiOutput("izbor.vojna")
-            ),
-            mainPanel(DT::dataTableOutput("voj"))
-            )),
-    tabItem(tabName = "stat",
-            fluidRow(
-            )),
-    tabItem(tabName = "komentar",
-            fluidRow( textInput("komentar", "Dodaj svoje mnenje", "Tvoje mnenje"),
-                      verbatimTextOutput("value"),
-                      DT::dataTableOutput("komentiranje")
+shinyUI(fluidPage(theme = shinytheme("cerulean"),
+                  
+                  
+                  titlePanel("Iskalnik po vseh vojnah od leta 1800"),
+                  
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel("Iskanje po sodelujocih",
+                               
+                               sidebarPanel(
+                                 uiOutput("izbor.sodelujoci")
+                               ),
+                               mainPanel(DT::dataTableOutput("sodel"), width=12)
                       
-            ))
-    ))
-    
-fluidPage(useShinyjs(),
-           conditionalPanel(condition = "output.signUpBOOL!='1' && output.signUpBOOL!='2'",#&& false", 
-                   vpisniPanel),       # UI panel za vpis
-  conditionalPanel(condition = "output.signUpBOOL=='1'", registracijaPanel),  # UI panel registracija
-  conditionalPanel(condition = "output.signUpBOOL=='2'",    # Panel, ko si ze vpisan
-                   dashboardPage(#dashboardHeader(disable=T),
-                     dashboardHeader(title = "Pregled vojn",
-                                                 tags$li(class = "dropdown",
-                                                         tags$li(class = "dropdown", textOutput("dashboardLoggedUser"), style = "padding-top: 15px; padding-bottom: 15px; color: #fff;"),
-                                                         tags$li(class = "dropdown", actionLink("dashboardLogin", textOutput("logintext")))
-                                                         )),
-                                 sidebar,
-                                 body,
-                                 skin = "blue")),
-  theme="bootstrap.css"
-)
+                      
+                      
+                      
+                    ),
+                      tabPanel("Iskanje po vojnah",
+                               
+                               sidebarPanel(
+                                 uiOutput("izbor.vojna")
+                               ),
+                               mainPanel(DT::dataTableOutput("voj"), width=12)
+                      ))
+                      
+                      
+                      
+                  )
+                  
+))
+
+
+
